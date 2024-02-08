8ccd2e016a4db269a5b0de4d8851a44c581ad59f
Przemysław Kitowski <kitowski.przemek@gmail.com>
2024-02-08 12:04:03 +0100

    Add navigation menu component

---
+++ components/Navbar.js
@@ -0,0 +1,173 @@
+'use client'
+import { useState } from 'react'
+import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar'
+import { Button } from './ui/button'
+
+const menu = [
+  {
+    label: 'Home',
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='m20.893 13.393-1.135-1.135a2.252 2.252 0 0 1-.421-.585l-1.08-2.16a.414.414 0 0 0-.663-.107.827.827 0 0 1-.812.21l-1.273-.363a.89.89 0 0 0-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.212.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 0 1-1.81 1.025 1.055 1.055 0 0 1-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.655-.261a2.25 2.25 0 0 1-1.383-2.46l.007-.042a2.25 2.25 0 0 1 .29-.787l.09-.15a2.25 2.25 0 0 1 2.37-1.048l1.178.236a1.125 1.125 0 0 0 1.302-.795l.208-.73a1.125 1.125 0 0 0-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 0 1-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 0 1-1.458-1.137l1.411-2.353a2.25 2.25 0 0 0 .286-.76m11.928 9.869A9 9 0 0 0 8.965 3.525m11.928 9.868A9 9 0 1 1 8.965 3.525'
+        />
+      </svg>
+    ),
+    href: '/',
+  },
+  {
+    label: 'Science',
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5'
+        />
+      </svg>
+    ),
+    href: '/',
+  },
+  {
+    label: 'Health',
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
+        />
+      </svg>
+    ),
+    href: '/',
+  },
+  {
+    label: 'Marketing',
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46'
+        />
+      </svg>
+    ),
+    href: '/',
+  },
+  {
+    label: 'Technology',
+    icon: (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6'>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25'
+        />
+      </svg>
+    ),
+    href: '/',
+  },
+]
+
+export const Navbar = () => {
+  const [isMenuActive, setIsMenuActive] = useState(true)
+
+  return (
+    <nav className=' bg-gray-100 dark:bg-slate-900'>
+      <div className='flex items-center justify-between flex-wrap px-4 py-4'>
+        <Avatar>
+          <AvatarImage src='/assets/avatar.jpg' />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Button
+          onClick={() => setIsMenuActive(!isMenuActive)}
+          variant='outline'
+          className='p-2'
+          size='icon'>
+          {isMenuActive ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'>
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M6 18 18 6M6 6l12 12'
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke-width='1.5'
+              stroke='currentColor'
+              class='w-40 h-40'>
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+              />
+            </svg>
+          )}
+        </Button>
+      </div>
+      <div
+        className={`${
+          isMenuActive ? 'block' : 'hidden'
+        } basis-[100%] bg-gray-50 dark:bg-gray-800`}>
+        <ul>
+          {menu.map((menuItem) => (
+            <li>
+              <div className='gap-6 px-8 py-3 border-b-[1px] border-b-gray-200 flex justify-start items-center dark:border-b-gray-900'>
+                <div className='w-[40px] h-[40px] rounded-md bg-white shadow-sm flex items-center justify-center border-[1px] dark:bg-gray-900'>
+                  {menuItem.icon}
+                </div>
+                <div className='font-semibold opacity-[.8]'>
+                  {menuItem.label}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
